0b7aa128f5d1dcab0c6d6873745b4d59e49da044
jangmark <jangmark1995@naver.com>
2023-01-12 17:08:51 +0900

    기초 commit

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Post
+
+def single_post_page(request, value):
+    post = Post.objects.get(pk=value)
+    return render(
+        request,
+        'blog/single_post_page.html',
+
+    {
+        'post' : post,
+    }
+    )
+
+    
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+    return render(
+        request,
+    'blog/index.html', 
+    {
+        'posts': posts
+    }
+    )
+
+# Create your views here.
