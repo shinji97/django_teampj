8ab5b3cbb7500892dd455fd2a5eef75e248c2d53
shinji97 <tlswl9929@naver.com>
2023-01-13 14:44:28 +0900

    변경

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('mg/', views.mg_page),
+    path('gw/', views.gw_page),
+    path('sj/', views.sj_page),
+    path('je/', views.je_page),
+    path('sy/', views.sy_page),
     path('<int:value>/', views.single_post_page),
     path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
 from .models import Post
 
+def mg_page(request):
+    return render(
+        request,
+        'blog/jangmaga.html',
+
+    )
+
+def gw_page(request):
+    return render(
+        request,
+        'blog/gwangwon.html',
+
+    )
+
+def sj_page(request):
+    return render(
+        request,
+        'blog/sujin.html',
+
+    )
+
+def je_page(request):
+    return render(
+        request,
+        'blog/shinji.html',
+
+    )
+
+def sy_page(request):
+    return render(
+        request,
+        'blog/seoyeon.html',
+
+    )
+
 def single_post_page(request, value):
     post = Post.objects.get(pk=value)
     return render(
